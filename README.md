537ecc918b93943cf427413706ed64890a24f95b
Piotr <piotr.drzymkowski@idego.pl>
2024-06-07 15:59:59 +0200

    Initial commit, project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fancy Music Library Project
---
+++ src/fml_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FmlApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fml_api"
